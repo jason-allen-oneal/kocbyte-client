c9e7e9f9cbb72ddb0bb41584a069a043dd5b9200
Jason O'Neal <jason.allen.oneal@gmail.com>
2024-10-31 21:27:52 -0400

    Create user.js

---
+++ user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name			KoCByte
+// @version			1
+// @description		A Kingdoms of Camelot Tracker
+// @include			*.facebook.com/kingdomsofcamelot*
+// @grant			unsafeWindow
+// @grant			GM_deleteValue
+// @grant			GM_getValue
+// @grant			GM_setValue
+// @grant			GM_listValues
+// @grant			GM_xmlhttpRequest
+// @grant			GM_openInTab
+// @grant			GM_log
+// @grant			GM_addStyle
+// @grant			GM_registerMenuCommand
+// @require			http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @namespace kocbyte.com
+// ==/UserScript==
+
+//============================================================================
+
+(function() {
+  alert("hi");
+  var uW = unsafeWindow;
+  this.jQuery = jQuery.noConflict(true);
+})();
+
+
+
+
